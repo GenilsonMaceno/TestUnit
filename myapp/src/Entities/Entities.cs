8956dd412ba37abaa6bb1048f9544befddf09395
Genilson De Maceno <gmaceno@4bio.com.br>
2022-02-19 18:54:29 -0300

    Altered the parameters to english

---
+++ myapp/src/Entities/Entities.cs
@@ -0,0 +1,11 @@
+namespace src.Entities
+{
+    public class Entities
+    {
+        public int Entity { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Email { get; set; }
+    }
+}
