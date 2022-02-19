0150db8712101f9fe53e3ffe6af101c9b02031ee
Genilson De Maceno <gmaceno@4bio.com.br>
2022-02-18 23:12:12 -0300

    Adding the first entity

---
+++ myapp/src/Entities/Entidades.cs
@@ -0,0 +1,11 @@
+namespace src.Entities
+{
+    public class Entidades
+    {
+        public int Entidade { get; set; }
+        public string Nome { get; set; }
+        public string Sobrenome { get; set; }
+        public int Idade { get; set; }
+        public string Email { get; set; }
+    }
+}
